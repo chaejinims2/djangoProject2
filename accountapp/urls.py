24f1002a9bbb02593493189f5f7d592b29e2c689
chaej <chaejinims2@gmail.com>
2021-07-14 17:53:23 +0900

    2021-07-14 실습1 COMMIT 1 LOGIN/LOGOUT View

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,19 +1,18 @@
+from django.contrib.auth.views import LogoutView, LoginView
 from django.urls import path
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 
 app_name = 'accountapp'
 
 
-class AccountCreateView:
-    model = User
-    form_class = UserCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
-    template_name = 'accountapp/create.html'
-
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'),
+         name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 
     path('create/',AccountCreateView.as_view(), name='create')
 
