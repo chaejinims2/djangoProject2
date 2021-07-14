0122318ea614ec53491625322e2f8eb6b4d914cb
chaej <chaejinims2@gmail.com>
2021-07-14 17:12:05 +0900

    2021-07-14 실습2 COMMIT 1

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -4,7 +4,17 @@ from accountapp.views import hello_world
 
 app_name = 'accountapp'
 
+
+class AccountCreateView:
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/create.html'
+
+
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+
+    path('create/',AccountCreateView.as_view(), name='create')
 
 ]
