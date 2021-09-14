1f722035ba2fd18bfb6ff3e037e3ed1739eb03f6
chaejin <chaejinims2@gmail.com>
2021-09-14 11:22:57 +0900

    9/14 4ban class commit 1

--- djangoProject2/settings/base.py
+++ djangoProject2/settings/base.py
@@ -15,33 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from django.urls import reverse_lazy
 
-BASE_DIR = Path(__file__).resolve().parent.parent # 부모 경로를 찾는 것
-
-env_list = dict()
-
-local_env = open(os.path.join(BASE_DIR, '../.env'))
-
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n', '')
-    start = line.find('=')
-    key = line[:start]
-    value = line[start +1:]
-    env_list[key] = value
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
-# SECRET_KEY = 'django-insecure-m_%apm8v8g@$6%bda3g#*kk2&^5br1y9d@283b4)$!y09p4&js'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["*"]
+BASE_DIR = Path(__file__).resolve().parent.parent.parent # 부모 경로를 찾는 것
 
 
 # Application definition
